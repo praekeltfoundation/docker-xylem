61812881fbdef9770e0432168d0579794d96ffd0
Colin Alston <colin.alston@gmail.com>
2016-04-05 10:27:19 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="docker_xylem",
-    version='0.1.3',
+    version='0.1.4',
     url='http://github.com/praekeltfoundation/docker-xylem',
     license='MIT',
     description="A docker storage plugin",
