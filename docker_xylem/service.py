2c55492a7b6fcb2aadfa74f6ed8f9ea122aeccc9
Tinotenda Chemvura <ttchemvura@gmail.com>
2016-12-12 12:23:09 +0200

    Added new logs to service.py using twisted.logger Removed old logs which used twisted.python.log

--- docker_xylem/service.py
+++ docker_xylem/service.py
@@ -4,7 +4,7 @@ import cgi
 
 from twisted.internet import defer
 from twisted.web import server, resource
-from twisted.python import log
+from twisted.logger import Logger
 
 from docker_xylem import utils
 
@@ -12,6 +12,7 @@ from docker_xylem import utils
 class DockerService(resource.Resource):
     isLeaf = True
     addSlash = True
+    log = Logger()
 
     def __init__(self, config):
         self.requestRouter = {
@@ -82,16 +83,22 @@ class DockerService(resource.Resource):
 
             if (path in err) and ("not mounted" in err):
                 # Return false is not mounted
+                self.log.warn('Volume {path} is not mounted', path=path)
                 defer.returnValue(False)
             else:
                 # Raise an exception for any other error
                 raise Exception(err)
         else:
             # Return true is mounted
+            self.log.info('Successfully unmounted {path}', path=path)
             defer.returnValue(True)
 
     @defer.inlineCallbacks
     def mount_volume(self, request, data):
+        self.log.debug(
+            'Function Call: mount_volume(), `data` = \"{data}\"',
+            data=str(data)
+        )
         name = data['Name']
         path = os.path.join(self.mount_path, name)
 
@@ -100,17 +107,28 @@ class DockerService(resource.Resource):
 
             if name not in self.current:
                 self.current[name] = path
-
+            self.log.info(
+                'Successfully mounted volume {name}. Mount path:\"{path}\"',
+                name=name, path=path
+            )
             defer.returnValue({
                 "Mountpoint": path,
                 "Err": None
             })
 
         except Exception, e:
+            self.log.error(
+                'Error mounting {name}. \"{e.message}\"',
+                name=name, e=e
+            )
             defer.returnValue({"Err": repr(e)})
 
     @defer.inlineCallbacks
     def unmount_volume(self, request, data):
+        self.log.debug(
+            'Function Call: unmount_volume(), `data` = \"{data}\"',
+            data=str(data)
+        )
         name = data['Name']
         path = os.path.join(self.mount_path, name)
 
@@ -119,13 +137,32 @@ class DockerService(resource.Resource):
                 defer.returnValue({"Err": None})
             else:
                 # Try older mount paths
+                self.log.warn(
+                    'Volume {name} was not found on \"{path}\".' +
+                    'Now trying older paths.',
+                    name=name, path=path
+                )
                 paths = self.get_paths(name)
                 for path in paths:
-                    yield self._umount_fs(path)
+                    self.log.info(
+                        'Attemptting to unmount {name} from \"{path}\"',
+                        name=name, path=path
+                    )
+                    if (yield self._umount_fs(path)):
+                        # Return if the volume is found on older paths
+                        defer.returnValue({"Err": None})
 
+                self.log.warn(
+                    'Volume {name} not mounted on older mount paths.',
+                    name=name
+                )
                 defer.returnValue({"Err": None})
 
         except Exception, e:
+            self.log.error(
+                'Error unmounting volume {name}. \"{e.message}\"',
+                name=name, e=e
+            )
             defer.returnValue({"Err": repr(e)})
 
     def get_paths(self, name):
@@ -140,6 +177,10 @@ class DockerService(resource.Resource):
         return paths
 
     def get_volume_path(self, request, data):
+        self.log.debug(
+            'Function Call: get_volume_path(), `data` = \"{data}\"',
+            data=str(data)
+        )
         name = data['Name']
         path = os.path.join(self.mount_path, name)
         return {
@@ -149,11 +190,18 @@ class DockerService(resource.Resource):
 
     def remove_volume(self, request, data):
         # FIXME: This probably isn't supposed to do nothing.
-
+        self.log.debug(
+            'Function Call: remove_volume(), `data` = \"{data}\"',
+            data=str(data)
+        )
         return {"Err": None}
 
     @defer.inlineCallbacks
     def create_volume(self, request, data):
+        self.log.debug(
+            'Function Call: create_volume(), `data` = \"{data}\"',
+            data=str(data)
+        )
         name = data['Name']
 
         result = yield self.xylem_request('gluster', 'createvolume', {
@@ -161,13 +209,19 @@ class DockerService(resource.Resource):
         })
 
         if not result['result']['running']:
+            self.log.error('Error creating volume {name}', name=name)
             err = "Error creating volume %s" % name
         else:
             err = None
 
+        self.log.info('Successfully created the volume {name}.', name=name)
         defer.returnValue({"Err": err})
 
     def get_volume(self, request, data):
+        self.log.debug(
+            'Function Call: get_volume(), `data` = \"{data}\"',
+            data=str(data)
+        )
         name = data['Name']
 
         if name in self.current:
@@ -183,6 +237,10 @@ class DockerService(resource.Resource):
             return {'Err': 'No mounted volume'}
 
     def list_volumes(self, request, data):
+        self.log.debug(
+            'Function Call: list_volumes(), `data` = \"{data}\"',
+            data=str(data)
+        )
         vols = []
 
         for k, v in self.current.items():
@@ -194,7 +252,10 @@ class DockerService(resource.Resource):
         return {'Volumes': vols, 'Err': None}
 
     def capabilities(self, request, data):
-
+        self.log.debug(
+            'Function Call: capabilities(), `data` = \"{data}\"',
+            data=str(data)
+        )
         return {
             "Capabilities": {
                 "Scope": "global"
@@ -202,6 +263,11 @@ class DockerService(resource.Resource):
         }
 
     def plugin_activate(self, request, data):
+        self.log.debug(
+            'Function Call: plugin_activate(), `data` = \"{data}\"',
+            data=str(data)
+        )
+        self.log.info('Docker-Xylem plugin activated.')
         return {
             'Implements': ['VolumeDriver']
         }
@@ -222,6 +288,10 @@ class DockerService(resource.Resource):
         method = self.requestRouter.get(request.path)
 
         if not method:
+            self.log.warn(
+                '{request.path} is not implemented by plugin.',
+                request=request
+            )
             return "Not Implemented"
 
         return defer.maybeDeferred(method, request, data)
@@ -229,7 +299,7 @@ class DockerService(resource.Resource):
     def render_POST(self, request):
         request.setHeader("content-type", "application/json")
 
-        log.msg(request.path)
+        self.log.info('request.path', request=request)
 
         self._route_request(request).addCallback(self.completeCall, request)
 
