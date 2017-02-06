a64167f758661b0dfb44e8923b60d9973da34d51
Jeremy Thurgood <jeremy@praekelt.com>
2017-02-06 14:51:07 +0200

    Bump version post-release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="docker_xylem",
-    version='0.1.5',
+    version='0.1.6a0',
     url='http://github.com/praekeltfoundation/docker-xylem',
     license='MIT',
     description="A docker storage plugin",
