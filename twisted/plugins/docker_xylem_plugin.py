578fe6f7403de0f93b3ca2776092e5dfe8dbfa73
Jeremy Thurgood <jeremy@praekelt.com>
2017-02-16 16:33:57 +0200

    Create /run/docker/plugins before using it. (@bearnard)

--- twisted/plugins/docker_xylem_plugin.py
+++ twisted/plugins/docker_xylem_plugin.py
@@ -2,7 +2,7 @@ import yaml
 
 from zope.interface import implements
 
-from twisted.python import usage
+from twisted.python import filepath, usage
 from twisted.plugin import IPlugin
 from twisted.application.service import IServiceMaker
 from twisted.application import internet
@@ -25,9 +25,12 @@ class DockerServiceMaker(object):
 
     def makeService(self, options):
         config = yaml.load(open(options['config']))
+        sockfp = filepath.FilePath("/run/docker/plugins/xylem.sock")
+        if not sockfp.parent().exists():
+            sockfp.parent().makedirs()
 
-        return internet.UNIXServer(config.get(
-            'socket', "/run/docker/plugins/xylem.sock"),
+        return internet.UNIXServer(
+            config.get('socket', sockfp.path),
             server.Site(service.DockerService(config)))
 
 
